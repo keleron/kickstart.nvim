7ad189aa885a7e37da13871ef6bdd732234084fa
keleron <keleronx@gmail.com>
2025-04-22 01:30:18 -0400

    mover colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -23,5 +23,5 @@ return { -- You can easily change to a different colorscheme.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.cmd.colorscheme 'tokyonight-night'
-  end
+  end,
 }
